e358415d84db4eee391a7b32d3bbb4e2d3b277fc
Steve Tollaksen <41160954+WilliamTIII@users.noreply.github.com>
2020-03-11 11:49:21 -0700

    added

---
+++ babyshark.ps1
@@ -0,0 +1,3 @@
+﻿
+$var=echo Baby Daddy Mommy Grandpa Grandma|%{,(($a="$_ Shark")+" doo"*6)*3;"$a!"};Add-Type -AssemblyName System.Speech;($SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer).speak($var)
+
